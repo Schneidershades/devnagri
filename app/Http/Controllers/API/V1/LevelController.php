cfbcdbf43515d6f110870a9c910059ca9bc9df7b
Schneider Shades <schneidershades@gmail.com>
2021-07-22 17:00:47 +0100

    rename

--- app/Http/Controllers/API/V1/LevelController.php
+++ app/Http/Controllers/API/V1/LevelController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API\V1;
+namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Requests\LevelCreateFormRequest;
 use App\Http\Requests\LevelUpdateFormRequest;
--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\API\V1;
+namespace App\Http\Controllers\Api\v1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\UserRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Gate;
 
 
-class UserController extends BaseController
+class UserController extends Controller
 {
     public function __construct()
     {
