55d8e48a52a533cf86116c19585a5e2e78ac11ed
Schneider Shades <schneidershades@gmail.com>
2021-07-22 17:13:47 +0100

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 use App\Models\Role;
+// use Laravel\Sanctum\HasApiTokens;
 use App\Traits\Permissions\HasPermissionsTrait;
 
 class User extends Authenticatable // implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, HasApiTokens, HasPermissionsTrait;
+    use HasFactory, Notifiable, HasPermissionsTrait;
 
     /**
      * The attributes that are mass assignable.
